8b555005868aba5f5ecb82f1471f4bdc824ecdbd
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-12-26 14:35:31 +0100

    feat: finish basic read/write cycle

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,43 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 int main()
 {
     std::string inpath = "../Hallway.bmp";
     std::string outpath = "../Out.bmp";
 
-    std::ifstream infile(inpath, std::ios::in | std::ios::binary);
-    if (!infile.is_open()) {
-        std::cout << "Couldn't open " << inpath;
+    std::ifstream ifile(inpath, std::ios::in | std::ios::binary);
+    if (!ifile.is_open()) {
+        std::cout << "Couldn't open input file " << inpath << std::endl;
         return EXIT_FAILURE;
     }
 
     BitmapFileHeader_t fileHeader {};
     BitmapInfoHeader_t infoHeader {};
-    infile.read(reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
-    infile.read(reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader));
+    ifile.read(reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
+    ifile.read(reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader));
 
-    infile.seekg(fileHeader.OffBits / 8, std::ios::beg);
+    ifile.seekg(fileHeader.OffBits, std::ios::beg);
     Image image(infoHeader.Width, infoHeader.Height);
     for (int i = 0; i < infoHeader.Height; i++) {
-        infile.read(reinterpret_cast<char*>(&(image.Pixels[i])), infoHeader.Width * sizeof(RGBTriple));
-        infile.seekg(image.Padding, std::ios::cur);
+        ifile.read(reinterpret_cast<char*>(&(image.Pixels[i][0])), infoHeader.Width * sizeof(RGBTriple));
+        ifile.seekg(image.Padding(), std::ios::cur);
+    }
+
+    std::ofstream ofile(outpath, std::ios::out | std::ios::binary);
+    if (!ofile.is_open()) {
+        std::cout << "Couldn't open output file " << outpath << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    ofile.write(reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
+    ofile.write(reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader));
+    for (int i = 0; i < infoHeader.Height; i++) {
+        ofile.write(reinterpret_cast<char*>(&(image.Pixels[i][0])), infoHeader.Width * sizeof(RGBTriple));
+        for (int j = 0; j < image.Padding(); j++) {
+            ofile.write("\0", sizeof(BYTE));
+        }
     }
 
     return EXIT_SUCCESS;
