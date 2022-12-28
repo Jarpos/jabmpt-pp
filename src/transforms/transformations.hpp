6d6fa18584b03faab40e1bbe2a70864a9b61be36
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-12-28 01:54:00 +0100

    ref: add ActionDescription and rename Description to HelpDescription

--- src/transforms/transformations.hpp
+++ src/transforms/transformations.hpp
@@ -15,13 +15,14 @@ typedef std::function<void(Image&)> transformation_t;
 
 struct Transformation {
     std::function<void(Image&)> Function;
-    const char* const Description;
+    const char* const HelpDescription;
+    const char* const ActionDescription;
 };
 
 const std::unordered_map<char, Transformation> Transformations = {
-    { 'w', { BlackAndWhite, "Makes image black and white" } },
-    { 'x', { FlipX, "Flips the image on the X axis (right to left)" } },
-    { 'y', { FlipY, "Flips the image on the Y axis (upside down)" } },
+    { 'w', { BlackAndWhite, "Makes image black and white", /************/ "Made black and white" } },
+    { 'x', { FlipX, "Flips the image on the X axis (right to left)", /**/ "Flipped on X-Axis" } },
+    { 'y', { FlipY, "Flips the image on the Y axis (upside down)", /****/ "Flipped on Y-Axis" } },
 };
 
 } // namespace transform
