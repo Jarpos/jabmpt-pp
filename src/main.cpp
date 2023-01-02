80d31bad4239797114542eafa22fb324f0270171
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-01-02 04:30:10 +0100

    feat: add ability to choose what's edited with cmdline

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "types.hpp"
 #include "transforms/transformations.hpp"
+#include "types.hpp"
 
 #include <fstream>
 #include <iostream>
@@ -8,20 +8,51 @@
 Image ReadImageFromBmp(const std::string& inpath);
 void WriteImageToBmp(const std::string& path, const Image& image);
 
+void PrintHelp();
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Given amount of args was not correct" << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::string inpath = argv[1];
-    std::string outpath = argv[2];
-
+    std::string inpath = argv[1], outpath = argv[2];
     Image image = ReadImageFromBmp(inpath);
-    transforms::Transformations.at('w').Function(image);
-    transforms::Transformations.at('y').Function(image);
-    transforms::Transformations.at('x').Function(image);
+
+    PrintHelp();
+    std::string input;
+    std::cout << "> ";
+    std::getline(std::cin, input);
+    while (input != "") {
+        const auto& t = transforms::Transformations.find(input[0]);
+
+        if (t == transforms::Transformations.end()) {
+            std::cout << "Couldn't find function " << input[0] << "\n";
+        } else {
+            t->second.Function(image);
+            std::cout << t->second.ActionDescription << "\n";
+        }
+
+        std::cout << "> ";
+        std::getline(std::cin, input);
+    }
+
     WriteImageToBmp(outpath, image);
 
     return EXIT_SUCCESS;
 }
+
+void PrintHelp() {
+    for (const auto& k : transforms::Transformations) {
+        std::cout << k.first << ": " << k.second.HelpDescription << "\n";
+    }
+    std::cout << "\n";
+}
+
+// transforms::Transformations.at('b').Function(image);
+// transforms::Transformations.at('w').Function(image);
+// transforms::Transformations.at('y').Function(image);
+// transforms::Transformations.at('x').Function(image);
+// transforms::Transformations.at('r').Function(image);
+// transforms::Transformations.at('g').Function(image);
+// transforms::Transformations.at('b').Function(image);
