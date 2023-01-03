8ab9cf14d6477f66fcee6b2f49508e9f590643fe
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-01-03 05:02:33 +0100

    docs: add explanation for B&W func

--- src/transforms/blackwhite.cpp
+++ src/transforms/blackwhite.cpp
@@ -3,6 +3,9 @@
 namespace transforms {
 
 void BlackAndWhite(Image& image) {
+    // Yes, I know that actual human color perception is
+    // slightly scewed when it comes to Black & White
+    // pictures, but I don't care about this atm
     image.ForEachPixel([&](RgbValue& r) {
         r.SetAll(r.Average());
     });
