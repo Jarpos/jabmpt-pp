3f3a198aa2715bcd610ed7638b8a0c0a1875baac
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-01-03 04:03:46 +0100

    ref: use ForEachPixel instead of normal for iterators

--- src/transforms/channels.cpp
+++ src/transforms/channels.cpp
@@ -5,27 +5,21 @@
 namespace transforms {
 
 void DisableRedChannel(Image& image) {
-    for (auto& line : image.Pixels) {
-        std::for_each(line.begin(), line.end(), [&](RgbValue& pixel) {
-            pixel.Red = 0;
-        });
-    }
+    image.ForEachPixel([&](RgbValue& r) {
+        r.Red = 0;
+    });
 }
 
 void DisableGreenChannel(Image& image) {
-    for (auto& line : image.Pixels) {
-        std::for_each(line.begin(), line.end(), [&](RgbValue& pixel) {
-            pixel.Green = 0;
-        });
-    }
+    image.ForEachPixel([&](RgbValue& r) {
+        r.Green = 0;
+    });
 }
 
 void DisableBlueChannel(Image& image) {
-    for (auto& line : image.Pixels) {
-        std::for_each(line.begin(), line.end(), [&](RgbValue& pixel) {
-            pixel.Blue = 0;
-        });
-    }
+    image.ForEachPixel([&](RgbValue& r) {
+        r.Blue = 0;
+    });
 }
 
 } // namespace transforms
