18a06908937c2a0e665a9e36af671c9cc7e98d2f
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-01-09 15:46:02 +0100

    ref: improve performance (from ~1.5s to ~200ms) by using GetSurroundingQuick function

--- src/transforms/blur.cpp
+++ src/transforms/blur.cpp
@@ -8,14 +8,15 @@ namespace transforms {
 void Blur(Image& image) {
     Image copy(image);
 
+    std::array<RgbValue, 9> surrounding {};
     for (int y = 0; y < image.Height(); y++) {
         for (int x = 0; x < image.Width(); x++) {
             double r(0), g(0), b(0);
-            const auto& cells = copy.GetSurrounding(y, x);
-            for (const auto& cell : cells) {
-                r += (double)cell.Red / cells.size();
-                g += (double)cell.Green / cells.size();
-                b += (double)cell.Blue / cells.size();
+            int count = copy.GetSurroundingQuick(y, x, surrounding);
+            for (int i = 0; i < count; i++) {
+                r += (double)surrounding[i].Red / count;
+                g += (double)surrounding[i].Green / count;
+                b += (double)surrounding[i].Blue / count;
             }
             image.At(y, x).Set(std::round(r), std::round(g), std::round(b));
         }
