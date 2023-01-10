fb96b1cc22b1fe71725e939f6b547378de8993d5
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-01-10 22:08:36 +0100

    fix: fix naming to make compileable

--- src/transforms/edgedetection.cpp
+++ src/transforms/edgedetection.cpp
@@ -16,7 +16,7 @@ void EdgeDetection(Image& image) {
         -2, +0, +2, //
         -1, +0, +1, //
     };
-    std::array<short, 9> kernelY = {
+    std::array<short, 9> kernelX = {
         -1, -2, -1, //
         +0, +0, +0, //
         +1, +2, +1, //
