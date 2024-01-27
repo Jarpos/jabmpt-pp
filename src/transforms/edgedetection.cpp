052317cc71f752d7e6a3025edcac49a700af13c7
Jarpos <51173894+Jarpos@users.noreply.github.com>
2024-01-27 23:28:14 +0100

    implement basic edgedetection algorithm (overflow problems for now)

--- src/transforms/edgedetection.cpp
+++ src/transforms/edgedetection.cpp
@@ -5,18 +5,20 @@
 
 namespace transforms {
 
+RgbValue Convolute(const std::array<uint8_t, 9>& const matrix, const Image& const image, int y, int x);
+
 // See:
 // https://cs50.harvard.edu/x/2021/psets/4/filter/more/#edges
 // https://en.wikipedia.org/wiki/Sobel_operator
 void EdgeDetection(Image& image) {
     Image copy(image);
 
-    std::array<short, 9> kernelY = {
+    std::array<uint8_t, 9> kernelY = {
         -1, +0, +1, //
         -2, +0, +2, //
         -1, +0, +1, //
     };
-    std::array<short, 9> kernelX = {
+    std::array<uint8_t, 9> kernelX = {
         -1, -2, -1, //
         +0, +0, +0, //
         +1, +2, +1, //
@@ -25,8 +27,31 @@ void EdgeDetection(Image& image) {
     std::array<RgbValue, 9> surrounding {};
     for (int y = 0; y < image.Height(); y++) {
         for (int x = 0; x < image.Width(); x++) {
+            RgbValue gy = Convolute(kernelY, copy, y, x);
+            RgbValue gx = Convolute(kernelX, copy, y, x);
+
+            // TODO: Rework to prevent overflows
+            image.At(y, x).Set((gy.Square() + gx.Square()).Sqrt());
         }
     }
+}
+
+RgbValue Convolute(const std::array<uint8_t, 9>& const matrix, const Image& const image, int y, int x) {
+    RgbValue outpixel(0, 0, 0);
+
+    int i = 0;
+    for (int yc = -1; yc < 2; yc++) {
+        for (int xc = -1; xc < 2; xc++, i++) {
+            if (y + yc >= 0 && y + yc < image.Height()) {
+                if (x + xc >= 0 && x + xc < image.Width()) {
+                    // TODO: maybe add += operator overload
+                    outpixel = outpixel + image.At(y + yc, x + xc) * matrix[i];
+                }
+            }
+        }
+    }
+
+    return outpixel;
 }
 
 } // namespace transforms
--- src/transforms/transformations.hpp
+++ src/transforms/transformations.hpp
@@ -11,10 +11,11 @@ void Blur(Image& image);
 void BlackAndWhite(Image& image);
 void FlipX(Image& image);
 void FlipY(Image& image);
+void Invert(Image& image);
+void EdgeDetection(Image& image);
 void DisableRedChannel(Image& image);
 void DisableGreenChannel(Image& image);
 void DisableBlueChannel(Image& image);
-void Invert(Image& image);
 
 typedef std::function<void(Image&)> transformation_t;
 
@@ -30,6 +31,7 @@ const std::unordered_map<char, Transformation> Transformations = {
     { 'x', { FlipX, "Flips the image on the X axis (right to left)", /**/ "Flipped on X-Axis" } },
     { 'y', { FlipY, "Flips the image on the Y axis (upside down)", /****/ "Flipped on Y-Axis" } },
     { 'i', { Invert, "Inverts the r, g and b channels bitwise", /*******/ "Inverted all channels" } },
+    { 'e', { EdgeDetection, "Applies Sobel operator edge detection", /**/ "Applied Sobel operator" } },
 
     { 'r', { DisableRedChannel, "Disables the red channel", /***********/ "Disabled Red channel" } },
     { 'g', { DisableGreenChannel, "Disables the Green channel", /*******/ "Disabled Green channel" } },
